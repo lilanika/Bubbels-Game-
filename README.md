578832b91785484dbc21b1cd1d447ab1e1724a2f
Vero <nikonova@tmail.de>
2021-01-26 19:14:21 +0100

    day2

--- README.md
+++ README.md
@@ -8,18 +8,30 @@
 3. create a Player
 4. create Bubbles for a player to pop und score points.
 4.1 create function handleBubbles()
-5. Animation Loop where all of that will bring the game to life.
-6. add sound to the bubbles. 
+5. function handleBackground(){ 
+6. Animation Loop where all of that will bring the game to life.
+7. add sound to the bubbles. 
+The bubbles have two sounds. In the constructor of the class Bubbel both sounds are assigned randomly, with the ternary operator when a new bubble is created. 
+another if statement in the handleBubbles function plays the assigned sound,when player and a bubble has a collision
+soundeffects: www.opengameart.org
+
+8. Player Image
 
 ##### Day 2
+1. background image 
+2. player image 
+3. rotation of the fish 
+4. bubble image
+5. Readme 
+
 
 ## 1. canvas setup  
-
 ## 2 mouse Interaction & coordinates
-
-###### getBoundingClientRect(); 
-    Ohne diese Methode wird der Abstand von der Kante des Fensters und nicht von den Kanten des Bildschirmelements gemessen. 
-    Mit dieser Methode wird die aktuelle Größe und Position des Canvas-Elements gemessen. 
+###### Get the right mouse coordinates 
+with getBoundingClientRect();  wird der Abstand von der Kante des Fensters und nicht von den Kanten des Bildschirmelements gemessen. 
+Mit dieser Methode wird die aktuelle Größe und Position des Canvas-Elements gemessen. 
+###### even if the screen is displayed in a reduced size
+EventListener'resize' = resize window to right mouse position 
 
 ###### create mouse object with his startposition
 ###### create two eventlisteners mousedown & mouseup
@@ -29,7 +41,7 @@
 .> addEventListener ('mouseup') takes two arguments: type of event to listen for wich is mouseup and callback Function set the booleanvalue to false.
 
 # 3 create a class of Player
-######  properties
+###### properties
 
 -> startposition = this.x || this.y
 -> radius = 30
@@ -38,16 +50,26 @@
 -> frame will keep track of number of frames and the current position we are animating.
 
 ###### Methods 
-###### Update() 
-    this Method update Player position to move the player towards the mouse. It compares player current position and current mouse position. 
-    If they don't have the same y and x coordinates,the function should measure the distance and subtract the distance from player position.
-    It also takes care how fast the Object takes the position of the mouse  /30 
+#### Update() 
+#### move player towards the mouse
+     in this Method the Player position will updated towards the mouse. It compares player current position and current mouse position. If they don't have the same y and x coordinates,the function should measure the distance and subtract the distance from player position. It also takes care how fast the Object takes the position of the mouse  /30
+
+##### how the fish will always face the mouse
+    calculation of theta wich is the counterclockwise angle between x acsis and any point. Cause update method runs over and over this angle will be recalulated for every frame of the game. 
+        
 
 ######  draw()
     The purpose of a draw method is to update the screen in milliseconds. This is the reason why on the screen you can see the movements. 
+##### show images and change the images depending on the direction 
+   1. In Player draw() method the if statement checking if the mouse position is smaller or larger than the player position.and executes one of the two drawImage() methods. 
+  
+  The drawImage() method manages which image is displayed and how it behaves in terms of size and scalability. 
+   > save() saves current canvas settings
+   > translate() reflect curretly player position
+   > rotate(this.angle)
+   > restore() reset all translate and rotate cores, to the  last call of ctx safe = so just the player changing, nothing others. 
 
-    Here it shows a red circle. 
-    Also, with each mouse click, a line is created that starts at the player object and ends at the coordinates of the mouse pointer.  The player object  move to the  new X and y coordinates calculated in the Update() function. 
+
 # 4. create Bubbles for a player to pop und score points.
 
 ###### create a bubbleArray 
@@ -72,42 +94,48 @@ For this we use the ternary operator.it's a one line if else stateme
 
 2.2 Methods
 ######  update() 
-// move bubbles up.  Each bubble move in negative direction on y-axis 
+
+##### move bubbles up.  
+
+Each bubble move in negative direction on y-axis 
 depending on their individual speed. 
-// calculate distance between player and a bubble
-######  draw()
-2.2 draw a blue circle
+##### calculate distance between player and a bubble
 
+###### draw()
+###### show Bubble IMG 
 
-# 4.1 create function handleBubbles()
-
-######  create every 50 frame rates a new Bubble Object
+# 4.1 function handleBubbles()
+###### create every 50 frame rates a new Bubble Object
 This function should create every 50 frame rates a new Bubble Object 
-and push it to the bubblesArray The gameFrame can be used to add periodic 
-events to our game and has his invoking place in the animation function. 
-The new Bubble trigger the constructor that is in
-the Bubble class and create a new Bubble object */ 
-###### iterate throw bubblesArray. 
- Each bubble call Update() and the draw method. 
+and push it to the bubblesArray The gameFrame can be used to add periodic events to our game and has his invoking place in the animationfunction. The new Bubble trigger the constructor that is in
+the Bubble class and create a new Bubble object  
+###### Each bubble call Update() and the draw method.  
+ iterate throw bubblesArray.
 ######  prevent the array from growing endlessly.
- If this bubble vertical y position is less than 0, than it has moved pass the top of canvas, call splice method. Splice cut out every Element a certain/bestimmten index from the Array 
-######  check if player and a bubble has a collision
- that's happen when the distance is less than 80 
-###### if this. Counted is false
-//increment the score by one 
-//set counted to true
-// remove this bubble from the array 
-*/ 
+ If this bubble vertical y position is less than 0, than it has moved pass the top of canvas, call splice method.
+ if so remove with Splice. Decrementing i by one for the correct index 
+ ###### check if if player and a bubble has a collision
+That's happen when the distance is less than 80.
+else if Is executed only for existing bubbles not the removed. 
+###### if there is a collision 
+-> increment the score by one 
+-> Play one of the sounds
+-> set counted to true
+-> remove this bubble from the array 
+-> Decrementing i by one for the correct index 
 
 
+# 5 function handleBackground()
 
-
+This function handle the background image 
+every time this function runs it decrease the firstImg_x by one, the 
+background (firstImg_x) sliding to the left.if firstImg_x is less than - BG.width (canvas width)so if it has completely disappeared behind the left edge of canvas,set firstImg_x to BG.width again.Same thing happens with the second image, secondImg_x.
 # 6. animation Loop 
 
 ###### clearing the entire canvas from old paint with clearRect 
 ###### calling handleBubbles()
 ###### calling player.update();
-######  show the current score 
-######  increment gameFrame++ by one 
+###### show the current score 
+###### increment gameFrame++ by one 
 ######  creating an animation loop = requestAnimationFrame(animate);
 through a principle that called recursion, where a function calls itself over an over.
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-/////////// 1. Canvas Setup ///////////////////////////////////
+/////////// 1. Canvas Setup //////////////////////
 
 const canvas = document.getElementById('canvas1');
 // it will need getContext, to have access to build in 2d drawing methods 
@@ -7,17 +7,25 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800; 
 canvas.height = 500; 
 
+//Global variables 
+
 // Score increased by one for every bubble that the fish caught
 let score = 0; 
 //GameFrame increased by one for every animation Loop.
 let gameFrame = 0; 
 ctx.font = '50px Georgia'; 
-
+let gameSpeed = 1; 
 
 /////////////////2. Get the Mouse coordinates ////////////////
 
 let canvasPosition = canvas.getBoundingClientRect(); 
 // console.log(canvasPosition)
+
+//resize window to right mouse position
+window.addEventListener('resize', function (){
+    canvasPosition = canvas.getBoundingClientRect(); 
+
+});
 
 // create mouse object with his startposition
 const mouse =  { 
@@ -38,79 +46,110 @@ canvas.addEventListener('mouseup',function(){
     mouse.click = false; 
 }) 
 
-//////////////////////3.create player///////////////////// 
+//////////////////////3.create player/////////////////////
 
+// Playerimages 
+const playerLeft = new Image();
+playerLeft.src = 'images/fish_swim_left.png ';
+
+const playerRight = new Image(); 
+playerRight.src ='images/fish_swim_right.png';
 
 class Player {
     constructor(){
         //startposition 
-        this.x = canvas.width;    // manage left and right.
-        this.y = canvas.height / 2; // half of the height. 
+        this.x = canvas.width;    // direction from left or right
+        this.y = canvas.height/2; // half of the height 
         this.radius = 30; 
-        //will rotate the player towards current mouse position 
+        // player rotate around in a circle to face the mouse
+        // calculte theta wich is the counterclockwise angle between x acsis and any point. 
         this.angle = 0; 
+        //for the images
         this.frameX = 0; 
         this.frameY = 0; 
         this.frame = 0;  
+        //the position of the frame, that we need from the sheet.  
         this.spriteWidth = 498; 
         this.spriteHeight = 327; 
     }
-   
+
     update() { 
          //calculate distance betweend player und mouse 
-        const distancePlayerX = this.x -mouse.x;
-        const distancePlayerY = this.y -mouse.y; 
+        const distanceXplayerMouse = this.x -mouse.x;
+        const distanceYplayerMouse  = this.y -mouse.y; 
+        // to rotate the fishposition 
+        let theta = Math.atan2(distanceYplayerMouse, distanceXplayerMouse )
+        this.angle = theta 
         // change the current player position  
         if (mouse.x !== this.x) { 
-            this.x -= distancePlayerX /20 ; // speed of movemend 
+            this.x -= distanceXplayerMouse /20 ; // speed of movemend 
         } 
         if( mouse.y !== this.y) {
-            this.y -= distancePlayerY/20;
+            this.y -= distanceYplayerMouse/20;
         }
     }
     draw(){
-        if(mouse.click) {
-            //line 
-            ctx.lineWidth = 0.2;
-            ctx.beginPath(); 
-            // starting position of the line. 
-            ctx.moveTo(this.x, this.y);
-            //endposition of line 
-            ctx.lineTo(mouse.x, mouse.y);
-            //stroke connect the two points
-            ctx.stroke();
-        }
-      
-        ctx.fillStyle = 'red'; 
-        //for the circle
-        ctx.beginPath(); 
-        //form
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI *2);
-        //fill draw the circle 
-        ctx.fill(); 
-        ctx.closePath(); 
-    }
-}
 
+       // save current canvas settings
+       ctx.save(); 
+       // reflect curretly player position
+       ctx.translate(this.x , this.y); 
+       ctx.rotate(this.angle);
+
+
+        if(this.x >= mouse.x){
+            ctx.drawImage(playerLeft, 
+                // crop out area
+                this.frameX * this.spriteWidth,
+                this.frameY * this.spriteHeight,
+                this.spriteWidth, 
+                this.spriteHeight,
+                // position 
+                0 - 60, 
+                0 - 45,
+                //scale
+                this.spriteWidth /4,
+                this.spriteHeight /4 ); 
+
+        }   else {
+            ctx.drawImage(playerRight, 
+                this.frameX * this.spriteWidth,
+                this.frameY * this.spriteHeight,
+                this.spriteWidth,
+                this.spriteHeight,
+                0 - 60,
+                0 - 45,
+                this.spriteWidth / 4,
+                this.spriteHeight /4 ); 
+        } 
+        // reset all translate and rotate cores, 
+        //to the last call of ctx.safe()
+        ctx.restore()
+    }
+} 
 //create Player Object 
 const player = new Player()
 
 
 ////////////// 4. create Bubbles ////////////////////
-const bubblesArray = [];
 
+const bubblesArray = [];
+//Bubble Image 
+const bubbleImage = new Image(); 
+bubbleImage.src = 'images/bubble_pop_frame_01.png'; 
 class Bubble {
     constructor(){
+
         this.x = Math.random() * canvas.width;
         this.y = canvas.height + 100 ;
         this.radius = 50;
         //a rendom number between 1 and 6 
         this.speed = Math.random() * 5 + 1;
-        
+
         this.distance; 
-        // add only one point per bubble to the score. 
+        // add only one point per bubble count to the score. 
         this.counted = false; 
-        //
+        //ternary operator. assigned randomly
         this.sound = Math.random() <= 0.5 ? 'sound1':'sound2' ; 
      }
    
@@ -124,24 +163,22 @@ class Bubble {
         this.distance = Math.sqrt(distanceBubbleX * distanceBubbleX + distanceBubbleY * distanceBubbleY);
     }
     draw() {
-        //draw a blue circle
-        ctx.fillStyle = 'blue'; 
-        ctx.beginPath(); 
-        ctx.arc( this.x, this.y, this.radius, 0, Math.PI *2); 
-        ctx.fill();
-        ctx.closePath();
-        ctx.stroke();
-        
+       //BubbleImage 
+        ctx.drawImage( bubbleImage , 
+        this.x -68,
+        this.y -68,
+        this.radius * 2.7, 
+        this.radius * 2.7
+        ) 
     }
 }
 
-////////////////////// 4.1 Handle Bubbles /////////////////////////
+////////////////////// 4.1 function handleBubbles() ////////////
 
 const bubblePop1 = document.createElement('audio');
-bubblePop1.src = 'Plop.ogg';
+bubblePop1.src = 'sound/Plop.ogg';
 const bubblePop2 = document.createElement('audio');
-bubblePop2.src = 'bubbles-single2.wav';
-
+bubblePop2.src = 'sound/bubbles-single2.wav';
 
 function handleBubbles() {
   
@@ -149,54 +186,96 @@ function handleBubbles() {
         bubblesArray.push(new Bubble());
         //console.log(bubblesArray.length);
     }
-  
+
     for(let i = 0; i < bubblesArray.length; i++) {
-        //each element calling both of his method
+        //each element calling this methods 
         bubblesArray[i].update(); 
         bubblesArray[i].draw();   
-    }
-
-    
-    for (let i = 0; i < bubblesArray.length; i++){
-
-    
-/* if one Bubble moved past the top edge of canvas, remove this bubble. with .radius * 2 prevents the bubbles from disappearing from the screen too early */
+        //check if bubble has disappeared over the top edge
         if(bubblesArray[i].y < 0 - bubblesArray[i].radius * 2){
-            bubblesArray.splice(i,1);
-        }
+         //if so remove 
+         bubblesArray.splice(i,1);
+         //for the correct index 
+         i--;
 
-        // check if player and a bubble has a collision, thats happen when the distance is less then 80 
-        if (bubblesArray[i].distance < bubblesArray[i].radius + player.radius) {   
-        //console.log('collision');
 
-        // only if this.counted is false, increment the score by one 
+    } else if 
+    (bubblesArray[i].distance < bubblesArray[i].radius + player.radius) {   
+
         if(!bubblesArray[i].counted) {
-
-            if (bubblesArray[i].sound == 'sound1') {
-                bubblePop1.play();
-            } else 
+        if (bubblesArray[i].sound == 'sound1') {
+            bubblePop1.play();
+        } else 
             bubblePop2.play();
-            score++;
-           
+            score ++;
             bubblesArray[i].counted = true;
-            // remove this bubble from the array 
             bubblesArray.splice(i, 1);
-        }      
+             i--;
         }
-        
     }
-
+   }
 }
 
- 
 /////////////////////// animation Loop //////////////////////////
-
 /* Here we calling all functions */
+/*
+###### clearing the entire canvas from old paint with clearRect 
+###### calling handleBubbles()
+###### calling player.update();
+######  show the current score 
+######  increment gameFrame++ by one 
+######  creating an animation loop = requestAnimationFrame(animate);
+through a principle that called recursion, where a function calls itself over an over. */ 
+
+
+
+
+// Repeating Background 
+const background = new Image(); 
+background.src = 'images/D8GEAg5.png'; 
+
+const BG = { 
+    //first img 
+    firstImg_x: 0, 
+    //second img so right
+    secondImg_x: canvas.width, 
+    y: 0,
+    width: canvas.width, 
+    height: canvas.height
+}
+
+//////////////function handleBackground() ////////////////
+
+function handleBackground(){ 
+
+    BG.firstImg_x -= gameSpeed;
+    if (BG.firstImg_x < - BG.width) {
+    BG.firstImg_x= BG.width;
+    }
+    BG.secondImg_x -= gameSpeed;
+    if (BG. secondImg_x < -BG.width) {
+    BG.secondImg_x = BG.width;
+    }
+    ctx.drawImage(
+        background,
+        BG.firstImg_x,
+        BG.y,
+        BG.width, 
+        BG.height );
+
+    ctx.drawImage(
+        background, 
+        BG.secondImg_x, 
+        BG.y, 
+        BG.width, 
+        BG.height );    
+}
   
 function animate( ) {
     //clearing the entire canvas from old paint. 
     ctx.clearRect( 0, 0, canvas.width, canvas.height);
     // calling bubbles function. 
+    handleBackground();
     handleBubbles();
     //calling the player-update Method to calculate player position
     player.update();
@@ -215,4 +294,6 @@ function animate( ) {
 }
 
 animate()
+
+
 
